0560ba5359c72b80c485240555ab7d48d9c6e75c
Robert Muscalu <robytzurobert15@yahoo.com>
2021-04-24 10:19:20 +0300

    Added new class commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,9 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-
+        Teacher teacher = new Teacher("Marian","Informatica",30,2.5);
+        Teacher teacher1 = new Teacher("Constantinopol","Istorie",56, 1.0);
+        System.out.println(teacher1);
+        System.out.println(teacher);
     }
 }
