88d30991398bbc3d5fb5455c16760c4d881a686a
Your Name <gridan.silviu@yahoo.com>
2021-04-24 11:24:37 +0300

    Implemented Student class

--- src/com/company/Students.java
+++ src/com/company/Students.java
@@ -1,2 +1,26 @@
-package com.company;public class Students {
+package com.company;
+
+public class Students {
+    private String name;
+    private String grupa;
+    private String curs;
+    private float notaSemestriala1;
+    private float notaSemestriala2;
+    private float mediaAnuala;
+
+    public Students(String name,String grupa,String curs,  float notaSemestriala1, float notaSemestriala2){
+        this.name = name;
+        this.grupa = grupa;
+        this.curs = curs;
+        this.notaSemestriala1 = notaSemestriala1;
+        this.notaSemestriala2 = notaSemestriala2;
+        }
+    public String getName() { return name; }
+    public String getGrupa() { return grupa; }
+    public String getCurs() { return curs; }
+    public float getNotaSemestriala1() { return notaSemestriala1; }
+    public float getNotaSemestriala2() { return notaSemestriala2;}
+    public float getMediaAnuala() { return mediaAnuala; }
+    public void mediaAnuala() { mediaAnuala = (notaSemestriala1 + notaSemestriala2)/2; }
 }
+
